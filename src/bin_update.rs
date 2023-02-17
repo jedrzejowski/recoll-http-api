fd772a07a2691fb72afbdc1204b75f230b02aa7c
Adam Jędrzejowski <adam@jedrzejowski.pl>
2023-02-17 22:42:37 +0100

    dalsze prace

--- src/bin_update.rs
+++ src/bin_update.rs
@@ -21,10 +21,23 @@ async fn main() -> Result<()> {
   for (name, file_index) in &config.indexes {
     let mut child = file_index.spawn_update_process().await?;
 
-    let mut lines = BufReader::new(child.stderr.take().unwrap()).lines();
+    let mut reader = BufReader::new(child.stderr.take().unwrap());
+    let mut buf = vec![];
 
-    while let Some(line) = lines.next().await {
-      println!("{} | {}", name, line?);
+    while reader.read_until(b'\n', &mut buf).await? > 0 {
+      let line = String::from_utf8_lossy(&buf);
+      println!("{} | {}", name, &line[0..(line.len() - 1)]);
+      buf.clear();
+    }
+
+    let status = child.status().await?;
+    println!("{} | {}", name, status);
+
+    match status.code() {
+      Some(code) => {
+        println!("{} | exited with status code: {}", name, code)
+      },
+      None => println!("Process terminated by signal"),
     }
   }
 
--- src/endpoint_query.rs
+++ src/endpoint_query.rs
@@ -1,27 +1,27 @@
-use actix_web::{web, HttpResponse};
-use serde::{Deserialize};
-use crate::index_repo::{IndexRepo};
 use crate::file_index::FileIndexQueryOptions;
+use crate::index_repo::IndexRepo;
+use actix_web::{web, HttpResponse};
+use serde::Deserialize;
 
 #[derive(Debug, Deserialize)]
 pub struct MyPath {
-  pub index_name: String
+  pub index_name: String,
 }
 
 pub async fn handler(
-    path: web::Path<MyPath>,
-    payload: web::Query<FileIndexQueryOptions>,
-    app_config: web::Data<IndexRepo>,
+  path: web::Path<MyPath>,
+  payload: web::Query<FileIndexQueryOptions>,
+  app_config: web::Data<IndexRepo>,
 ) -> actix_web::Result<HttpResponse> {
   let MyPath { index_name } = path.into_inner();
 
-  let file_index = app_config.get_index(index_name)
-    .ok_or(actix_web::error::ErrorNotFound("not found"))?;
+  let file_index =
+    app_config.get_index(index_name).ok_or(actix_web::error::ErrorNotFound("not found"))?;
 
-  let results = file_index.query(payload.into_inner())
+  let results = file_index
+    .query(payload.into_inner())
     .await
     .map_err(|_err| actix_web::error::ErrorInternalServerError("internal server error"))?;
-
 
   Ok(HttpResponse::Ok().json(results))
 }
